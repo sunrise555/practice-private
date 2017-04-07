6d68c0fc7fed632547f8164b114e9b2e04216e67
Sunny <810710788@qq.com>
2017-04-07 19:18:02 +0800

    输出改动

--- src/sunny/homework/decorator/Test.java
+++ src/sunny/homework/decorator/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		TokenStream ts = new MyTokenStream(System.in); // 标准输入的适配器
 		while (ts.getToken().tokenType != Token.TokenType.NONE) {
-			System.out.println(ts.getToken());
+			System.out.print(ts.getToken() + " ");
 			ts.consumeToken();
 		}
 	}
